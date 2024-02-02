1620cd0ce6f9887c83fd5a407024fe57be601a72
AndreyShabunin1999 <81184064+AndreyShabunin1999@users.noreply.github.com>
2024-02-02 21:33:03 +0300

    Сверстаны xml основной и экран с созданием заметки. Также реализованы переходы между ними.

---
+++ app/src/main/java/com/anshabunin/sketchnote/ui/CreateNoteFragment.kt
@@ -0,0 +1,28 @@
+package com.anshabunin.sketchnote.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.anshabunin.sketchnote.databinding.FragmentCreateNoteBinding
+
+class CreateNoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentCreateNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCreateNoteBinding.inflate(inflater, container, false)
+
+        binding.imageBack.setOnClickListener {
+            view?.findNavController()?.navigateUp()
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/anshabunin/sketchnote/ui/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.anshabunin.sketchnote.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.anshabunin.sketchnote.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        binding.imageAddNote.setOnClickListener {
+            view?.findNavController()?.navigate(MainFragmentDirections.openCreateNoteFragment())
+        }
+
+        return binding.root
+    }
+}
